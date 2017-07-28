bc9a3b4172adc462f9a351f6aa33793ff578c0a3
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-28 13:10:26 +0100

    Update Employee.cs

--- CRUDWebApi/Models/Employee.cs
+++ CRUDWebApi/Models/Employee.cs
@@ -13,7 +13,9 @@ namespace CRUDWebApi.Models
         [Required(ErrorMessage = "Name is required")]
         [StringLength(160)]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Age is required")]
         public int Age { get; set; }
+        [Required(ErrorMessage = "Salary is required")]
         public int Salary { get; set; }
 
 
